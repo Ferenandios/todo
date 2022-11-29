a6120dd8e57a3890f9e6e974ed4efe36ab3cb54c
Ferenandios <www.terunplay@gmail.com>
2022-11-29 18:18:25 +0500

    Coded ToDo list

--- src/components/Content/Todos/Todos.jsx
+++ src/components/Content/Todos/Todos.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Todo from './Todo/Todo'
 
-const Todos = () => {
+const Todos = ({ setList }) => {
+  const list = React.createRef()
+  setList(list.current)
   return (
-    <div className='mt-8 flex flex-col items-center'>
-      <Todo />
-      <Todo />
-      <Todo />
+    <div ref={list} className='mt-8 flex flex-col items-center'>
       <Todo />
     </div>
   )
