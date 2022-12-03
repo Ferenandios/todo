2d91ec3eb06cd05af53334f1393e3e81c925d639
Ferenandios <www.terunplay@gmail.com>
2022-12-03 15:50:14 +0500

    Connected module scss

--- src/components/Content/Todos/Todo/Todo.jsx
+++ src/components/Content/Todos/Todo/Todo.jsx
@@ -1,21 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { AiOutlineCheck } from 'react-icons/ai'
+import styles from './Todo.module.scss'
 
 const Todo = ({text}) => {
+  const currentButton = useRef()
+  const currentSpan = useRef()
   const [isChecked, setIsChecked] = useState(false)
   const checkedBox = (<div className='h-10 w-10 border-blue-400 border-[3px] rounded-lg relative'><span className='flex justify-center absolute -top-4 -left-[6px] items-center w-[170%] h-[170%] text-8xl'><AiOutlineCheck /></span></div>)
   const uncheckedBox = (<div className='h-10 w-10 border-blue-400 border-[3px] rounded-lg'></div>)
   return (
-    <button 
-    className='outline-none flex items-center mt-5 w-[90%] p-3 rounded-2xl bg-black'
+    <button
+    ref={currentButton} 
+    className={styles.button}
     onClick={() => {
       setIsChecked(!isChecked)
     }}
     >
       {isChecked ? checkedBox : uncheckedBox}
-      <span className='ml-4 text-xl whitespace-nowrap text-ellipsis overflow-hidden'>{isChecked ? <s>{text}</s> : text}</span>
+      <div className='flex ml-4 w-full'>
+        <span 
+        ref={currentSpan}
+        className={[styles.span, ' max-w-[360px]'].join('')}
+        >{isChecked ? <s>{text}</s> : text}
+        </span>
+      </div>
     </button>
-  )
+  ) // длина всей кнопки - 80px max-w-[354px]
 }
 
 export default Todo
